e75ea893e6771809afacd33174eb2f505c456b44
0xrcd <54696324+0xrcd@users.noreply.github.com>
2023-12-10 19:41:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
+echo 'src-git xiaoroujipsw https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git xiaoroujipswpkg https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
